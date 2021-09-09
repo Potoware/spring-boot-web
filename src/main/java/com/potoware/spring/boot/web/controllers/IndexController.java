f1c3eae4a25345729b4ed036df287cca72d22f9a
Alejandro Potosi <alejandropotosi@outlook.com>
2021-09-09 09:31:10 -0500

    Usando RequestMapping para solo ejecutar los metodos en ese nivel EJ RequestMapping

--- src/main/java/com/potoware/spring/boot/web/controllers/IndexController.java
+++ src/main/java/com/potoware/spring/boot/web/controllers/IndexController.java
@@ -3,15 +3,17 @@ package com.potoware.spring.boot.web.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 	
-	@GetMapping({"/index","/","/home"})
+	@GetMapping({"/index","/","/home",""})
 	public String index(Model model) {
 		
-		model.addAttribute("titulo", "Hola potoware");
+		model.addAttribute("titulo", "Hola potoware con model");
 		
 		return "index";
 	}
