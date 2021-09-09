de5d59ebcd5b072276b53a0584bb77f12469b8ae
Alejandro Potosi <alejandropotosi@outlook.com>
2021-09-09 17:23:34 -0500

    Spring Web - enviando parametros al Controller con 2 objetos de cualquier tipo {texto},{numero}

--- src/main/java/com/potoware/spring/boot/web/controllers/EjemploVariablesRutaController.java
+++ src/main/java/com/potoware/spring/boot/web/controllers/EjemploVariablesRutaController.java
@@ -9,6 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("variables")
 public class EjemploVariablesRutaController {
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute("titulo","enviar parametros de la ruta (@Patvariblee)");
+		return "variables/index";
+	}
 	
 	@GetMapping("/string/{texto}")
 	public String variables (@PathVariable String texto, Model model) {
@@ -17,5 +22,14 @@ public class EjemploVariablesRutaController {
 		return "variables/ver";
 		
 	}
+	@GetMapping("/string/{texto}/{textotwo}")
+	public String variables (@PathVariable String texto,@PathVariable String textotwo ,Model model) {
+		model.addAttribute("titulo","Recibir parametros de la ruta (@Patvariblee)");
+		model.addAttribute("resultado","el texto enviado en la ruta es: "+ texto+" y el numero de celular es: "+ textotwo);
+		
+		return "variables/ver";
+		
+	}
+
 
 }
