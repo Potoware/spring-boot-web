eec10fb6767e026be0842d8cc9db3d5930fd26f2
Alejandro Potosi <alejandropotosi@outlook.com>
2021-09-09 15:57:20 -0500

    Spring Web - Enviar parametros en el codigo usando HttpServletRequest

--- src/main/java/com/potoware/spring/boot/web/controllers/EjemploParamsController.java
+++ src/main/java/com/potoware/spring/boot/web/controllers/EjemploParamsController.java
@@ -1,5 +1,7 @@
 package com.potoware.spring.boot.web.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,5 +22,19 @@ public class EjemploParamsController {
 	public String index() {
 		
 		return "params/index";
+	}
+	
+	@GetMapping("/mix-params")
+	public String param(@RequestParam String saludo , @RequestParam Integer numero, Model model) {
+		model.addAttribute("resultado",saludo.concat(" Como estas?, recuerda que hoy es "+numero));
+		return "params/ver";
+	}
+	
+	@GetMapping("/mix-params-request")
+	public String param(HttpServletRequest request, Model model) {
+		String saludo = request.getParameter("saludo");
+		Integer numero = Integer.parseInt(request.getParameter("numero"));
+		model.addAttribute("resultado",saludo.concat(" Como estas?, recuerda que hoy es "+numero));
+		return "params/ver";
 	}
 }
